2a43fd1d5dbc424ba98ca758fcf625e19aaeeb98
Justin Rhea <jcrhea@southern.edu>
2021-01-14 09:41:09 -0500

    Initial File Upload to repo
    This is just to upload files from my local comp to my github repo

---
+++ IntroCSharpConsole/CSharpConsoleIntro.cs
@@ -0,0 +1,40 @@
+//ConsoleIntro App
+
+using System;
+using System.Collections;
+namespace CSharpConsoleIntro{
+    public class SortChallenge
+    {
+        public void swap(int indexOne, int indexTwo, int[] array){
+            if (indexOne < array.Length && indexTwo < array.Length){
+                int temp = array[indexOne];
+                array[indexOne] = array[indexTwo];
+            array[indexTwo] = temp;
+            }
+        }
+        public void Bubblesort(int[] array){
+            int LoopCounter = 0;
+            bool swapCheck = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                swapCheck = false;
+                for (int j = 0; j < array.Length-1; j++)
+                {
+                    if (array[j] > array[j+1]){
+                        swap(j, j+1, array);
+                        swapCheck = true;
+                    }
+                }
+                LoopCounter++;
+                if (swapCheck == false){
+                    break;
+                }
+            }
+            Console.WriteLine("Number of loops till sort stop at: " + LoopCounter);
+        }
+
+        public void BinarySearch(int start, int end, int[] searchArray){
+
+        }
+    }
+}
---
+++ IntroCSharpConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CSharpConsoleIntro;
+namespace IntroCSharpConsole
+{
+    public class Program
+    {
+
+         static void Main(string[] args)
+        {
+
+            SortChallenge sortingCheck = new SortChallenge();
+            // Console.WriteLine("Hey BunBun hehehe");
+            // var input = Console.ReadLine();
+            // Console.WriteLine("Your input is here: " + input);
+            var rand = new Random();
+
+            int[] generateList = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                 generateList[i] = rand.Next(1,10);
+            }
+            string outputList = string.Empty;
+            foreach (var item in generateList){
+                outputList = outputList + item.ToString() + ", ";
+            }
+            Console.WriteLine(outputList);
+            
+            sortingCheck.Bubblesort(generateList);
+
+            outputList = string.Empty;
+            foreach (var item in generateList){
+                outputList = outputList + item.ToString() + ", ";
+            }
+
+            Console.WriteLine(outputList);
+
+
+            // int[] listNum = {1, 2};
+            // foreach (var itemValue in listNum){
+            //     Console.WriteLine(itemValue);
+            // }
+
+        }
+        
+
+    }
+}
